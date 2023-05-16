9c549e81523d9be9a9c5c1c1b2a2f7f93d344f9d
JagadeeshArrvind <jagaapj64@gmail.com>
2023-05-16 14:53:41 +0530

    added get api in second controller class

--- TestForApplication02/src/main/java/com/example/dummy/second/demo/controller/SecondController.java
+++ TestForApplication02/src/main/java/com/example/dummy/second/demo/controller/SecondController.java
@@ -1,5 +1,6 @@
 package com.example.dummy.second.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/second")
 public class SecondController {
 
+	@GetMapping("/home")
+	public String getAddress() {
+		return "Chennai TNagar";
+		
+	}
 }
