4b3171381aca8b7c3f3ad5b3ffbc4ba8fdbe47ee
JagadeeshArrvind <89135878+jagaapj64@users.noreply.github.com>
2023-05-16 16:03:39 +0530

    Revert "added get api in second controller class"

--- TestForApplication02/src/main/java/com/example/dummy/second/demo/controller/SecondController.java
+++ TestForApplication02/src/main/java/com/example/dummy/second/demo/controller/SecondController.java
@@ -1,6 +1,5 @@
 package com.example.dummy.second.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/second")
 public class SecondController {
 
-	@GetMapping("/home")
-	public String getAddress() {
-		return "Chennai TNagar";
-		
-	}
 }
